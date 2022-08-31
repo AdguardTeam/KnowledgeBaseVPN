dd1d9abcf3f399894aa455d2361505c40ac8838b
Arseny Lisin <a.lisin@adguard.com>
2022-08-31 16:35:46 +0300

    add promo sticky bar to kb

---
+++ src/theme/StickyBar/StickyBar.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import { getCurrentPlatformName, utils, getOSName } from '@site/static/js/helpers';
+import { VPN_WEBSITE_URL, OS_NAMES } from '@site/static/js/consts';
+
+import s from './StickyBar.module.css';
+
+const OPENING_HEIGHT = 50;
+
+const getCurrentBtnText = () => {
+    switch (getOSName()) {
+        case OS_NAMES.MAC_OS:
+        case OS_NAMES.MAC:
+        case OS_NAMES.WINDOWS:
+            return (
+                <Translate
+                    id="sticky_bar.btn.text.download"
+                    description="Sticky bar btn text with label Download"
+                >
+                    Download
+                </Translate>
+            )
+        case OS_NAMES.IOS:
+            return (
+                <Translate
+                    id="sticky_bar.btn.text.ios"
+                    description="Sticky bar btn text with label App Store"
+                >
+                    Google Play
+                </Translate>
+            )
+        case OS_NAMES.ANDROID:
+            return (
+                <Translate
+                    id="sticky_bar.btn.text.android"
+                    description="Sticky bar btn text with label Google Play"
+                >
+                    Google Play
+                </Translate>
+            )
+        default:
+            return (
+                <Translate
+                    id="sticky_bar.btn.text.install"
+                    description="Sticky bar btn text with label Install"
+                >
+                    Install
+                </Translate>
+            )
+    }
+}
+
+const StickyBar = () => {
+    const iconClassName = `stickyBarBtnIcon_${getCurrentPlatformName()}`;
+
+    const [isVisible, setIsVisible] = useState(true);
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        const onScroll = (e) => {
+            setScrolling(e.target.documentElement.scrollTop > OPENING_HEIGHT);
+        };
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
+    const closeSticky = () => {
+        setIsVisible(false);
+    };
+
+    const sendStickyBarEvent = (event) => {
+        event.preventDefault();
+
+        utils.sendTrackingEvent(
+            "Indirect Install",
+            `Install ${getCurrentPlatformName()}`,
+            "knowledge_base",
+        );
+    }
+
+    return (
+        <>
+            {isVisible &&
+                (<div className={clsx(s.stickyBar, 'annoyance', {[s.stickyBarOpen]: scrolling })}>
+                    <div className={s.stickyBarIn}>
+                        <div className={s.stickyBarIntro}>
+                            <Translate
+                                id="sticky_bar.promo.free"
+                                description="String used for free GB promo in sticky bar"
+                            >
+                                Get 3 GB for free
+                            </Translate>
+                        </div>
+                        <div className={s.stickyBarBtnWrap}>
+                            <a
+                                className={s.stickyBarBtn}
+                                href={`${VPN_WEBSITE_URL}/download.html?auto=1`}
+                                onClick={sendStickyBarEvent}
+                            >
+                                <span className={clsx(s.stickyBarBtnIcon, s[iconClassName])} />
+
+                                {getCurrentBtnText()}
+                            </a>
+                            <div className={s.stickyBarBaloon}>
+                                <div className={s.stickyBarBaloonContent}>
+                                    <Translate
+                                        id="sticky_bar.btn.baloon.text"
+                                        description="String used for license agreement text in sticky bar"
+                                        values={{
+                                            eulaLink: (
+                                                <a href={`${VPN_WEBSITE_URL}/eula.html`}>
+                                                    <Translate
+                                                        id="sticky_bar.btn.baloon.text.link"
+                                                        description="The label for the link to license agreement"
+                                                    >
+                                                        License agreement
+                                                    </Translate>
+                                                </a>
+                                            ),
+                                        }}>
+                                        {'By downloading the program you accept the terms of the {eulaLink}'}
+                                    </Translate>
+                                </div>
+                            </div>
+                        </div>
+                        <button
+                            type='button'
+                            className={s.stickyBarCloseBtn}
+                            onClick={closeSticky}
+                        >
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default StickyBar;
---
+++ static/js/consts.js
@@ -0,0 +1,26 @@
+export const VPN_WEBSITE_URL = 'https://adguard-vpn.com';
+
+export const BROWSER_NAMES = {
+    EDGE: 'edge',
+    SAFARI: 'safari',
+    OPERA: 'opera',
+    CHROME: 'chrome',
+    FIREFOX: 'firefox',
+    YANDEX: 'yandex',
+};
+
+export const VPN_BROWSER_NAMES = {
+    EDGE: 'edge',
+    OPERA: 'opera',
+    CHROME: 'chrome',
+    FIREFOX: 'firefox',
+};
+
+export const OS_NAMES = {
+    WINDOWS: 'windows',
+    ANDROID: 'android',
+    IOS: 'ios',
+    MAC: 'mac',
+    MAC_OS: 'mac os',
+    LINUX: 'linux',
+};
